cbd14c2d0af0b6ef8754261206ce2730d38a0a27
Devin Coy <devinmcoy@gmail.com>
2025-06-02 19:59:44 -0400

    Refactor Temperature to match other class structure

--- SimpleUnits/Temperature.cs
+++ SimpleUnits/Temperature.cs
@@ -5,37 +5,66 @@
 /// </summary>
 public static class Temperature
 {
-    #region Conversion Factors
-    /// <summary>Converts Celsius to Fahrenheit.</summary>
-    private static double CelsiusToFahrenheit(double c) => (c * 9.0 / 5.0) + 32.0;
-    /// <summary>Converts Celsius to Kelvin.</summary>
-    private static double CelsiusToKelvin(double c) => c + 273.15;
-
-    /// <summary>Converts Fahrenheit to Celsius.</summary>
-    private static double FahrenheitToCelsius(double f) => (f - 32.0) * 5.0 / 9.0;
-    /// <summary>Converts Fahrenheit to Kelvin.</summary>
-    private static double FahrenheitToKelvin(double f) => (f - 32.0) * 5.0 / 9.0 + 273.15;
-
-    /// <summary>Converts Kelvin to Celsius.</summary>
-    private static double KelvinToCelsius(double k) => k - 273.15;
-    /// <summary>Converts Kelvin to Fahrenheit.</summary>
-    private static double KelvinToFahrenheit(double k) => (k - 273.15) * 9.0 / 5.0 + 32.0;
-    #endregion
-
+    /// <summary>
+    /// Static constructor to register temperature units and conversions with the <see cref="UnitConverter"/>.
+    /// </summary>
     static Temperature()
     {
         // Register Temperature unit
         UnitConverter.RegisterUnit(typeof(TemperatureUnit), BaseUnit.Temperature);
         // Celsius conversions
-        UnitConverter.RegisterConversion(TemperatureUnit.C, TemperatureUnit.F, CelsiusToFahrenheit);
-        UnitConverter.RegisterConversion(TemperatureUnit.C, TemperatureUnit.K, CelsiusToKelvin);
+        UnitConverter.RegisterConversion(TemperatureUnit.C, TemperatureUnit.F, CtoF);
+        UnitConverter.RegisterConversion(TemperatureUnit.C, TemperatureUnit.K, CtoK);
         // Fahrenheit conversions
-        UnitConverter.RegisterConversion(TemperatureUnit.F, TemperatureUnit.C, FahrenheitToCelsius);
-        UnitConverter.RegisterConversion(TemperatureUnit.F, TemperatureUnit.K, FahrenheitToKelvin);
+        UnitConverter.RegisterConversion(TemperatureUnit.F, TemperatureUnit.C, FtoC);
+        UnitConverter.RegisterConversion(TemperatureUnit.F, TemperatureUnit.K, FtoK);
         // Kelvin conversions
-        UnitConverter.RegisterConversion(TemperatureUnit.K, TemperatureUnit.C, KelvinToCelsius);
-        UnitConverter.RegisterConversion(TemperatureUnit.K, TemperatureUnit.F, KelvinToFahrenheit);
+        UnitConverter.RegisterConversion(TemperatureUnit.K, TemperatureUnit.C, KtoC);
+        UnitConverter.RegisterConversion(TemperatureUnit.K, TemperatureUnit.F, KtoF);
     }
+
+    /// <summary>
+    /// Converts a temperature from Celsius to Fahrenheit.
+    /// </summary>
+    /// <param name="c">The temperature in degrees Celsius.</param>
+    /// <returns>The equivalent temperature in degrees Fahrenheit.</returns>
+    private static double CtoF(double c) => (c * 9.0 / 5.0) + 32.0;
+
+    /// <summary>
+    /// Converts a temperature from Celsius to Kelvin.
+    /// </summary>
+    /// <param name="c">The temperature in degrees Celsius.</param>
+    /// <returns>The equivalent temperature in Kelvin.</returns>
+    private static double CtoK(double c) => c + 273.15;
+
+
+    /// <summary>
+    /// Converts a temperature from Fahrenheit to Celsius.
+    /// </summary>
+    /// <param name="f">The temperature in degrees Fahrenheit.</param>
+    /// <returns>The equivalent temperature in degrees Celsius.</returns>
+    private static double FtoC(double f) => (f - 32.0) * 5.0 / 9.0;
+
+    /// <summary>
+    /// Converts a temperature from Fahrenheit to Kelvin.
+    /// </summary>
+    /// <param name="f">The temperature in degrees Fahrenheit.</param>
+    /// <returns>The equivalent temperature in Kelvin.</returns>
+    private static double FtoK(double f) => (f - 32.0) * 5.0 / 9.0 + 273.15;
+
+    /// <summary>
+    /// Converts a temperature from Kelvin to Celsius.
+    /// </summary>
+    /// <param name="k">The temperature in Kelvin.</param>
+    /// <returns>The equivalent temperature in degrees Celsius.</returns>
+    private static double KtoC(double k) => k - 273.15;
+
+    /// <summary>
+    /// Converts a temperature from Kelvin to Fahrenheit.
+    /// </summary>
+    /// <param name="k">The temperature in Kelvin.</param>
+    /// <returns>The equivalent temperature in degrees Fahrenheit.</returns>
+    private static double KtoF(double k) => (k - 273.15) * 9.0 / 5.0 + 32.0;
 
     /// <summary>
     /// Converts a value from one temperature unit to another.
